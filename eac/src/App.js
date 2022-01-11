38e3b75dd6ac305d5b6727959389c5c706fcc62e
Tolulope Arinola <78857611+amazingrace17@users.noreply.github.com>
2022-01-11 17:45:40 +0100

    getting started

--- eac/src/App.js
+++ eac/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1> Login < /h1>
+      <h2>you are showing</h2>
+      <login />
+      <form>
+  <label>
+  <img>
+  </img>
+
+    <input type="text" name="name" />
+  </label> 
+  <input type="text" name="Submit" />
+  <h3>forgot Password</h3>
+  <button>
+    Login
+  </button>
+</form>
+
     </div>
   );
 }
---
+++ eac/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function login() {
+    return (
+        <div>
+            you are welcome
+        </div>
+    )
+}
+export default login
+
+
